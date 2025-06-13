5928ed0be5422ee3ef01f1b09a8d4991c5a1ae46
Precious Ekele <144235589+preciousekele@users.noreply.github.com>
2025-06-13 23:49:17 +0100

    Update verifyEmail.jsx

--- src/pages/verifyEmail.jsx
+++ src/pages/verifyEmail.jsx
@@ -1,28 +1,6 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import { useLocation, useNavigate } from 'react-router-dom';
-
-const VerifyEmail = () => {
-    const [message, setMessage] = useState('');
-    const [isLoading, setIsLoading] = useState(true);
-    const location = useLocation();
-    const navigate = useNavigate();
-
-    useEffect(() => {
-        // Extract the token from the URL query parameters
-        const params = new URLSearchParams(location.search);
-        const token = params.get('token');
-
-        if (!token) {
-            setMessage('No verification token found.');
-            setIsLoading(false);
-            return;
-        }
-
-        // Make a request to verify the email with the token
         const verifyEmail = async () => {
             try {
-                await axios.get(`http://localhost:5000/api/auth/verify-email?token=${token}`);
+                await axios.get(`https://sdars-backend.onrender.com/api/auth/verify-email?token=${token}`);
                 setMessage('Email verified successfully! Redirecting to login...');
                 setTimeout(() => {
                     navigate('/login');
