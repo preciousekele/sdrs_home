c170017f1a27728910a56602e09c0104d7e3d22d
Precious Ekele <144235589+preciousekele@users.noreply.github.com>
2025-06-18 22:40:51 +0100

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,13 +8,10 @@ import Hero from './hero/Hero';
 const Header = () => {
   return (
     <div className="home-container">
-      {/* Background elements */}
       <div className="bg-wrapper">
         <div className="gradient-overlay" />
         <div className="blur-overlay" />
       </div>
-
-      {/* Your existing content */}
       <header className="header" id="header">
         <nav className="nav container">
           <a href="#" className="nav__logo">
@@ -25,37 +22,9 @@ const Header = () => {
               MCPHERSON UNIVERSITY DISCIPLINARY RECORD SYSTEM
             </span>
           </a>
-
-          <div className="nav__menu" id="nav-menu">
-            <ul className="nav__list">
-              <li className="nav__item">
-                <a href="#home" className="nav__link active-link" style={{ color: "#FFF" }}>
-                  Home
-                </a>
-              </li>
-              <li className="nav__item" >
-                <a href="#" className="nav__link" style={{ color: "#FFF" }}>
-                  About
-                </a>
-              </li>
-              <li className="nav__item" >
-                <a
-                  href="/register"
-                  className="nav__link-button"
-                >
-                  E-PORTAL <i className="bx bx-log-in"></i>
-                </a>
-              </li>
-            </ul>
-
-            <div className="nav__close" id="nav-close">
-              <i className="ri-close-line"></i>
-            </div>
-          </div>
         </nav>
       </header>
       <Hero/>
-      {/* <GoalFeatures/> */}
     </div>
   );
 };
