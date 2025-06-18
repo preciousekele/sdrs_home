8ed18fec45018d6e559781c54796e8446cb78618
Precious Ekele <144235589+preciousekele@users.noreply.github.com>
2025-06-18 16:03:52 +0100

    Update Hero.jsx

--- src/pages/hero/Hero.jsx
+++ src/pages/hero/Hero.jsx
@@ -6,11 +6,7 @@ const Hero = () => {
     <div className="hero container">
       <div className="hero-text">
         <h1>Promoting Integrity and Transparency</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia qui
-          vitae saepe iure ut. Dolorum iusto labore fugiat eligendi vero soluta
-          aperiam atque esse ipsum maxime earum tempora, voluptatem ab.
-        </p>
+
         <button className="btn">
           <Link to="/register" className="nav__link-button">
             Go to E-Portal <i className="bx bx-log-in"></i>
